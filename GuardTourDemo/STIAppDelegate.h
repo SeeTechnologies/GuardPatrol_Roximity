4523861bca96f7b830fa35277f157f20a9000ed1
Lee Sitek <lsitek@seetechnologies.ca>
2014-07-16 21:51:12 -0600

    Initial Commit

---
+++ GuardTourDemo/STIAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  STIAppDelegate.h
+//  GuardTourDemo
+//
+//  Created by LS on 2014-07-16.
+//  Copyright (c) 2014 Seetechnologies Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface STIAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GuardTourDemo/STIViewController.m
@@ -0,0 +1,29 @@
+//
+//  STIViewController.m
+//  GuardTourDemo
+//
+//  Created by LS on 2014-07-16.
+//  Copyright (c) 2014 Seetechnologies Inc. All rights reserved.
+//
+
+#import "STIViewController.h"
+
+@interface STIViewController ()
+
+@end
+
+@implementation STIViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
